a4ed133f6b4f44a2af4b245973ca5ce3f43b50a5
miurakazuma67 <68637266+miurakazuma67@users.noreply.github.com>
2021-05-25 14:31:05 +0900

    簡単なモーダルを作成

---
+++ Presentation/ModalViewController.swift
@@ -0,0 +1,12 @@
+//
+//  ModalViewController.swift
+//  Presentation
+//
+//  Created by 三浦　一真 on 2021/05/25.
+//
+
+import UIKit
+
+class ModalViewController: UIViewController {
+
+}
--- Presentation/ViewController.swift
+++ Presentation/ViewController.swift
@@ -7,13 +7,21 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIViewControllerTransitioningDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
+    
+    @IBAction func modalAction(_ sender: AnyObject) {
+           let modalVC = self.storyboard?.instantiateViewController(withIdentifier: "modal")
+           modalVC!.modalPresentationStyle = .custom
+           modalVC!.transitioningDelegate = self
+           present(modalVC!, animated: true, completion: nil)
+       }
 
-
+       func presentationController(forPresented presented: UIViewController, presenting: UIViewController?, source: UIViewController) -> UIPresentationController? {
+           return PresentationController(presentedViewController: presented, presenting: presenting)
+       }
 }
 
